b8f6bca9c34abbe7a0467e448779495884927389
Alethea Givens <agivens2082@stu.neisd.net>
2016-02-03 12:09:46 -0600

    This is my slick game assignment.

--- src/aloneInTheLabyrinth/AloneInTheLabyrinth.java
+++ src/aloneInTheLabyrinth/AloneInTheLabyrinth.java
@@ -67,8 +67,6 @@ public class AloneInTheLabyrinth extends BasicGameState {
     public Orb powerOrb;
     
     public Player player;
-    
-    //public ArrayList<Orb> orblist = new ArrayList();
 
     public itemwin grabtowin;
 
@@ -83,21 +81,11 @@ public class AloneInTheLabyrinth extends BasicGameState {
     private static AppGameContainer app;
 
     private static Camera camera;
-    
-   // private Animation sprite, proup, prodown, proleft, proright;
 
     public static int counter = 0;
 
-    /**
-     *
-     * The collision map indicating which tiles block movement - generated based
-     *
-     * on tile properties
-     */
-	// changed to match size of sprites & map
     private static final int SIZE = 64;
 
-	// screen width and height won't change
     private static final int SCREEN_WIDTH = 1000;
 
     private static final int SCREEN_HEIGHT = 750;
@@ -113,59 +101,22 @@ public class AloneInTheLabyrinth extends BasicGameState {
 
         gc.setShowFPS(false);
 
-		// *******************
-		// Scenerey Stuff
-		// ****************
         forestMap = new TiledMap("res/agivens_4_map.tmx");
-
-		// Ongoing checks are useful
-        //System.out.println("Tile map is this wide: " + forestMap.getWidth());
 
         camera = new Camera(gc, forestMap);
 
-		// *********************************************************************************
-		// Player stuff --- these things should probably be chunked into methods
-        // and classes
-		// ********************************************************************************
-		// System.out.println("Horizontal count: "
-        // +runningSS.getHorizontalCount());
-		// System.out.println("Vertical count: " +runningSS.getVerticalCount());
-
-        
-
-		// *****************************************************************
-		// Obstacles etc.
-		// build a collision map based on tile properties in the TileD map
         blocked.blocked = new boolean[forestMap.getWidth()][forestMap.getHeight()];
-
-		// System.out.println("Map height:" + grassMap.getHeight());
-		// System.out.println("Map width:" + grassMap.getWidth());
-		// There can be more than 1 layer. You'll check whatever layer has the
-        // obstacles.
-		// You could also use this for planning traps, etc.
-		// System.out.println("Number of tile layers: "
-        // +grassMap.getLayerCount());
-        //System.out.println("The grassmap is " + forestMap.getWidth() + " by "
-                //+ forestMap.getHeight());
 
         for (int xAxis = 0; xAxis < forestMap.getWidth(); xAxis++) {
 
             for (int yAxis = 0; yAxis < forestMap.getHeight(); yAxis++) {
 
-				// int tileID = grassMap.getTileId(xAxis, yAxis, 0);
-				// Why was this changed?
-				// It's a Different Layer.
-				// You should read the TMX file. It's xml, i.e.,human-readable
-                // for a reason
                 int tileID = forestMap.getTileId(xAxis, yAxis, 1);
 
                 String value = forestMap.getTileProperty(tileID,
                         "blocked", "false");
 
                 if ("true".equals(value)) {
-
-                    //System.out.println("The tile at x axis " + xAxis + " and y axis "
-                            //+ yAxis + " is blocked.");
 
                     blocked.blocked[xAxis][yAxis] = true;
 
@@ -175,9 +126,6 @@ public class AloneInTheLabyrinth extends BasicGameState {
 
         }
 
-        //System.out.println("Array length " + blocked.blocked[0].length);
-
-		// A remarkably similar process for finding hostiles
         hostiles = new boolean[forestMap.getWidth()][forestMap.getHeight()];
 
         grabtowin = new itemwin(4400, 4400);
@@ -199,19 +147,14 @@ public class AloneInTheLabyrinth extends BasicGameState {
 
         camera.translateGraphics();
 
-		// it helps to add status reports to see what's going on
-		// but it gets old quickly
-		// System.out.println("Current X: " +player.x + " \n Current Y: "+ y);
         player.sprite.draw((int) Player.x, (int) Player.y);
 
-		//g.drawString("x: " + (int)player.x + "y: " +(int)player.y , player.x, player.y - 10);
         g.drawString("Health: " + Player.health, camera.cameraX + 10,
                 camera.cameraY + 10);
 
         g.drawString("speed: " + (int) (Player.speed * 10), camera.cameraX + 10,
                 camera.cameraY + 25);
 
-		//g.draw(player.rect);
         g.drawString("time passed: " + counter / 1000, camera.cameraX + 600, camera.cameraY);
 		// moveenemies();
 
@@ -222,15 +165,12 @@ public class AloneInTheLabyrinth extends BasicGameState {
         for (itemwin w : stuffwin) {
             if (w.isvisible) {
                 w.currentImage.draw(w.x, w.y);
-				// draw the hitbox
-                //g.draw(w.hitbox);
 
             }
         }
 
         for (Enemy e : monster) {
 
-                //System.out.println("The current selection is: " +e.currentanime);
             e.currentanime.draw(e.Bx, e.By);
         }
 
@@ -249,12 +189,10 @@ public class AloneInTheLabyrinth extends BasicGameState {
 
         double rightlimit = (forestMap.getWidth() * SIZE) - (SIZE * 0.75);
 
-		// System.out.println("Right limit: " + rightlimit);
         float projectedright = Player.x + fdelta + SIZE;
 
         boolean cangoright = projectedright < rightlimit;
 
-		// there are two types of fixes. A kludge and a hack. This is a kludge.
         if (input.isKeyDown(Input.KEY_UP)) {
 
             player.sprite = player.proup;
@@ -265,7 +203,6 @@ public class AloneInTheLabyrinth extends BasicGameState {
 
                 player.sprite.update(delta);
 
-				// The lower the delta the slower the sprite will animate.
                 Player.y -= fdelta;
 
             }
@@ -300,7 +237,6 @@ public class AloneInTheLabyrinth extends BasicGameState {
 
             player.sprite = player.proright;
 
-			// the boolean-kludge-implementation
             if (cangoright
                     && (!(isBlocked(Player.x + SIZE + fdelta,
                             Player.y) || isBlocked(Player.x + SIZE + fdelta, Player.y
@@ -310,8 +246,7 @@ public class AloneInTheLabyrinth extends BasicGameState {
 
                 Player.x += fdelta;
 
-            } // else { System.out.println("Right limit reached: " +
-            // rightlimit);}
+            }
 
         } else if (input.isKeyDown(Input.KEY_SPACE)) {
             
@@ -323,7 +258,6 @@ public class AloneInTheLabyrinth extends BasicGameState {
         for (itemwin w : stuffwin) {
 
             if (Player.rect.intersects(w.hitbox)) {
-                //System.out.println("yay");
                 if (w.isvisible) {
                     w.isvisible = false;
                     makevisible();
@@ -336,7 +270,6 @@ public class AloneInTheLabyrinth extends BasicGameState {
         for (Enemy m : monster) {
 
             if (Player.rect.intersects(m.ahitbox)) {
-                //System.out.println("yay");
                 if (m.isvisible) {
 
                     Player.health -= 1000;
@@ -374,7 +307,6 @@ public class AloneInTheLabyrinth extends BasicGameState {
 
         return blocked.blocked[xBlock][yBlock];
 
-		// this could make a better kludge
     }
 
 }
