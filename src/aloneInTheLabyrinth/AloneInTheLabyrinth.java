4bf446239aa3f80a7371acab6d7767ea534b4b8e
Alethea Givens <agivens2082@stu.neisd.net>
2016-02-04 12:40:30 -0600

    This is my slick game assignment.

--- src/aloneInTheLabyrinth/AloneInTheLabyrinth.java
+++ src/aloneInTheLabyrinth/AloneInTheLabyrinth.java
@@ -1,134 +1,80 @@
 package aloneInTheLabyrinth;
 
 import org.newdawn.slick.state.*;
-
 import java.io.IOException;
-
 import java.util.ArrayList;
-
 import java.util.Iterator;
-
 import java.util.logging.Level;
-
 import java.util.logging.Logger;
-
 import org.newdawn.slick.Animation;
 import org.newdawn.slick.Color;
-
 import org.newdawn.slick.AppGameContainer;
-
 import org.newdawn.slick.BasicGame;
-
 import org.newdawn.slick.Font;
-
 import org.newdawn.slick.GameContainer;
-
 import org.newdawn.slick.Graphics;
-
 import org.newdawn.slick.Image;
-
 import org.newdawn.slick.Input;
-
 import org.newdawn.slick.SlickException;
-
 import org.newdawn.slick.SpriteSheet;
-
 import org.newdawn.slick.TrueTypeFont;
-
 import org.newdawn.slick.geom.Rectangle;
-
 import org.newdawn.slick.geom.Shape;
-
 import org.newdawn.slick.state.BasicGameState;
-
 import org.newdawn.slick.state.StateBasedGame;
 import org.newdawn.slick.state.transition.FadeInTransition;
 import org.newdawn.slick.state.transition.FadeOutTransition;
-
 import org.newdawn.slick.tiled.TiledMap;
 import org.w3c.dom.css.Rect;
 
 class blocked {
 
     public static boolean[][] blocked;
-
     public static boolean[][] getblocked() {
-
         return blocked;
-
     }
-
-};
+}
 
 public class AloneInTheLabyrinth extends BasicGameState {
 
     public Enemy numberone;
-    
     public Orb magic8ball;
-    
     public Player player;
-
     public itemwin grabtowin;
-
     public ArrayList<itemwin> stuffwin = new ArrayList();
-
     public ArrayList<Enemy> monster = new ArrayList();
-
     private static TiledMap forestMap;
-
     private static AppGameContainer app;
-
     private static Camera camera;
-
     public static int counter = 0;
-
     private static final int SIZE = 64;
-
     private static final int SCREEN_WIDTH = 1000;
-
     private static final int SCREEN_HEIGHT = 750;
-
     public AloneInTheLabyrinth(int xSize, int ySize) {
-
     }
 
     public void init(GameContainer gc, StateBasedGame sbg)
             throws SlickException {
 
         gc.setTargetFrameRate(60);
-
         gc.setShowFPS(false);
-
         forestMap = new TiledMap("res/labyrinthMap.tmx");
-
         camera = new Camera(gc, forestMap);
 
         blocked.blocked = new boolean[forestMap.getWidth()][forestMap.getHeight()];
-
         for (int xAxis = 0; xAxis < forestMap.getWidth(); xAxis++) {
-
             for (int yAxis = 0; yAxis < forestMap.getHeight(); yAxis++) {
-
                 int tileID = forestMap.getTileId(xAxis, yAxis, 1);
-
                 String value = forestMap.getTileProperty(tileID,
                         "blocked", "false");
-
                 if ("true".equals(value)) {
-
                     blocked.blocked[xAxis][yAxis] = true;
-
                 }
-
             }
-
         }
-
         grabtowin = new itemwin(4400, 4400);
         stuffwin.add(grabtowin);
-        
         magic8ball = new Orb((int)Player.x + 10, (int)Player.y - 10);
-
         numberone = new Enemy(300, 300);
         monster.add(numberone);
     }
@@ -137,83 +83,52 @@ public class AloneInTheLabyrinth extends BasicGameState {
             throws SlickException {
 
         camera.centerOn((int) Player.x, (int) Player.y);
-
         camera.drawMap();
-
         camera.translateGraphics();
-
         player.sprite.draw((int) player.x, (int) player.y);
-
         g.drawString("Health: " + Player.health, camera.cameraX + 10,
                 camera.cameraY + 10);
-
         g.drawString("speed: " + (int) (Player.speed * 10), camera.cameraX + 10,
                 camera.cameraY + 25);
-
         g.drawString("time passed: " + counter / 1000, camera.cameraX + 600, camera.cameraY);
-
         if(magic8ball.isIsVisible()){
             magic8ball.orb.draw(magic8ball.getX(), magic8ball.getY());
         }
-        
         for (itemwin w : stuffwin) {
             if (w.isvisible) {
                 w.currentImage.draw(w.x, w.y);
-
             }
         }
-
         for (Enemy e : monster) {
-
             e.currentanime.draw(e.Bx, e.By);
         }
-
     }
 
     public void update(GameContainer gc, StateBasedGame sbg, int delta)
             throws SlickException {
-
+        
         counter += delta;
-
         Input input = gc.getInput();
-
         float fdelta = delta * Player.speed;
-
         Player.setpdelta(fdelta);
-
         double rightlimit = (forestMap.getWidth() * SIZE) - (SIZE * 0.75);
-
         float projectedright = Player.x + fdelta + SIZE;
-
         boolean cangoright = projectedright < rightlimit;
 
         if (input.isKeyDown(Input.KEY_UP)) {
-
             player.sprite = player.proup;
-
             float fdsc = (float) (fdelta - (SIZE * .15));
-
             if (!(isBlocked(Player.x, Player.y - fdelta) || isBlocked((float) (Player.x + SIZE + 1.5), Player.y - fdelta))) {
-
                 player.sprite.update(delta);
-
                 Player.y -= fdelta;
-
             }
-
         } else if (input.isKeyDown(Input.KEY_DOWN)) {
-
             player.sprite = player.prodown;
-
             if (!isBlocked(Player.x, Player.y + SIZE + fdelta)
                     || !isBlocked(Player.x + SIZE - 1, Player.y + SIZE + fdelta)) {
-
                 player.sprite.update(delta);
-
                 Player.y += fdelta;
-
             }
-
         } else if (input.isKeyDown(Input.KEY_LEFT)) {
             player.sprite = player.proleft;
             if (!(isBlocked(Player.x - fdelta, Player.y) || isBlocked(Player.x
@@ -272,13 +187,8 @@ public class AloneInTheLabyrinth extends BasicGameState {
     }
 
     private boolean isBlocked(float tx, float ty) {
-
         int xBlock = (int) tx / SIZE;
-
         int yBlock = (int) ty / SIZE;
-
         return blocked.blocked[xBlock][yBlock];
-
     }
-
 }
